9ed6cab59f9934612cf4a50daae5d5e6784e2e8b
Nikita Volokitin <n.a.volokitin@gmail.com>
2023-12-01 18:44:35 +0500

    Create a function page_main

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
-import json
-
 from flask import Flask
+
+from utils import loading_candidates
 
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def page_main():
+def page_main() -> str:
     """
     Возвращает главную страницу.
 
+    Вывод на главную страницу иформацию о всех кандидатах.
+
     Возвращаемое значение:
-            str: Информация о главной страницу.
+            str_candidates (str): Информация о всех кандидатах.
     """
-    return 'Главная страница'
+    candidates: list[dict] = loading_candidates()
+    str_candidates: str = '<pre>'
+    for candidate in candidates:
+        str_candidates += (
+            f'Имя: {candidate["name"]}\n'
+            f'Позиция: {candidate["position"]}\n'
+            f'Навыки: {candidate["skills"]}\n\n'
+        )
+    str_candidates += '</pre>'
+    return str_candidates
 
 
 if __name__ == '__main__':
