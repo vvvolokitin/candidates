41368c4063afa23e423a89b083aa416fba24b132
Nikita Volokitin <n.a.volokitin@gmail.com>
2023-12-01 17:57:31 +0500

    Create a function template page_main

--- main.py
+++ main.py
@@ -2,5 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+@app.route('/')
+def page_main():
+    """Возвращает главную страницу."""
+    return 'Главная страница'
+
+
 if __name__ == '__main__':
     app.run()
