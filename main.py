547d360cd683ee1b4bf641b9444e10c69c40f552
Nikita Volokitin <n.a.volokitin@gmail.com>
2023-12-01 19:36:39 +0500

    Create a function page_skill

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def page_main() -> str:
 
 
 @app.route('/candidate/<int:candidate_id>')
-def page_candidate(candidate_id) -> str:
+def page_candidate(candidate_id: int) -> str:
     """
     Возвращает страницу кандидата.
 
@@ -49,6 +49,29 @@ def page_candidate(candidate_id) -> str:
                 f'<br>Навыки: {candidate["skills"]}</pre>'
             )
     return result_candidate
+
+
+@app.route('/skill/<skill>')
+def page_skill(skill: str):
+    """
+    Возвращает страницу с кандидатами по навыкам.
+
+    Вывод на страницу иформацию о всех кандидатах с указанными навыками.
+
+    Возвращаемое значение:
+            str_candidates (str): Информация о кандидатах.
+    """
+    candidates: list[dict] = loading_candidates()
+    str_candidates: str = '<pre>'
+    for candidate in candidates:
+        if skill in [sk.lower() for sk in candidate['skills'].split(', ')]:
+            str_candidates += (
+                f'Имя: {candidate["name"]}\n'
+                f'Позиция: {candidate["position"]}\n'
+                f'Навыки: {candidate["skills"]}\n\n'
+            )
+    str_candidates += '</pre>'
+    return str_candidates
 
 
 if __name__ == '__main__':
